5fdd1089a535add5641eacde15ff5ea943c529aa
hyeonjin <11ghyeonjin@gachon.ac.kr>
2023-07-06 22:14:10 +0900

    [LoginPage] 로그인 레이아웃 구현 / [SignupPage] 회원가입 레이아웃 구현 / [ForgetPwPage] 비밀번호 찾기 레이아웃 구현 / [Navbar] 네비게이션 바 레이아웃 구현
    [LoginPage] 로그인 레이아웃 구현 /
    [SignupPage] 회원가입 레이아웃 구현 /
    [ForgetPwPage] 비밀번호 찾기 레이아웃 구현 /
    
    각각의 페이지 axios, js 코드 구현, navi 구현 안되어있음

---
+++ src/pages/ForgetPwPage.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useCallback } from "react";
+import signup_hand_image from "../assets/images/signupPage/signup_hand3.svg";
+import batton_logo_img from "../assets/images/batton_logo_auth.svg";
+
+export default function ForgetPwPage() {
+    return (
+      <div className="relative w-screen h-screen flex flex-col items-center justify-center overflow-hidden">
+        <img className="absolute z-0" src={signup_hand_image} style={{marginTop: '54vh', marginLeft: '-45vw'}}/>
+        <img className="relative z-10 mb-4" src={batton_logo_img}/>
+        <div className="flex flex-col space-y-6 relative z-10 items-center justify-center w-[38vw] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+          <form className="space-y-6 w-[30vw]">
+            <h4 className="text-xl font-medium text-gray-900">비밀번호 찾기</h4>
+            <div>
+              <label id="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="6oo@kakao.com" required />
+          </div>
+          <button type="submit" className="w-full text-white bg-[#5AAE8A] shadow-md hover:bg-[#285F43] focus:ring-4 focus:outline-none focus:ring-[#F9F9F9] font-medium rounded-lg text-sm px-5 py-2.5 text-center">로그인</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,41 @@
 export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary-1' : '#1D4D4F',
+      'primary-2' : '#285F43',
+      'primary-3' : '#4AA366',
+      'primary-4' : '#5AAE8A',
+      'primary-5' : '#DDF1E9',
+      'grey-1' : '#2E2D38',
+      'grey-2' : '#4D5661',
+      'grey-3' : '#6F8094',
+      'grey-4' : '#C4C4C4',
+      'grey-5' : '#DBDBDB',
+      'grey-6' : '#F4F4F4',
+      'error-1' : '#6B171F',
+      'error-2' : '#D62E3E',
+      'error-3' : '#E26D78',
+      'error-4' : '#F3C0C5',
+      'info-1' : '#C1A063',
+      'info-2' : '#FFDD2D',
+      'info-3' : '#FEE690',
+      'info-4' : '#FFF4CB',
+      'blue-1' : '#253365',
+      'blue-2' : '#2D81FF',
+      'blue-3' : '#90A8FE',
+      'blue-4' : '#CBCDFF',
+    },
+    extend: {
+      
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
