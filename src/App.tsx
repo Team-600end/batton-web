5917b50c83c681062793b806e6d97132cdf822c3
imae <hgyim00@naver.com>
2023-07-10 17:29:17 +0900

    BW-12 메인페이지 2차 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 // import { useState } from 'react';
-import Navbar from './components/Navbar';
-import './assets/index.css';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SingupPage';
-import ForgetPwPage from './pages/ForgetPwPage';
+import Navbar from "./components/Navbar";
+import "./assets/index.css";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SingupPage";
+import ForgetPwPage from "./pages/ForgetPwPage";
+import MainPage from "./pages/MainPage";
+import PjCard from "./components/PjCard";
+import DbMember from "./components/DbMember";
+import DbIssueLog from "./components/DbIssueLog";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Navbar/> */}
-      <LoginPage/>
+      <MainPage />
+      {/* <Navbar /> */}
+      {/* <LoginPage /> */}
       {/* <SignupPage/> */}
-      <ForgetPwPage/> 
+      {/* <ForgetPwPage /> */}
+
+      {/* ---아래는 지울것--- */}
+      {/* <PjCard /> */}
+      {/* <DbMember /> */}
+      {/* <DbIssueLog /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
