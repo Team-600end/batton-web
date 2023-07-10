04596fbaee224d08647e3642827c5871cc813e29
“lsh9295” <lsh929500@gmail.com>
2023-07-10 13:08:53 +0900

    BW-14 알림 컴포넌트 / 알림 전체보기 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import './assets/index.css';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SingupPage';
 import ForgetPwPage from './pages/ForgetPwPage';
+import Notice from './components/Notice';
+import NoticeDetailPage from './pages/NoticeDetailPage';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Navbar/> */}
-      <LoginPage/>
-      {/* <SignupPage/> */}
-      <ForgetPwPage/> 
+      <Navbar/>
+      {/*<NoticeDetailPage/>*/}
+      {/*<LoginPage/>*/}
+      {/*<SignupPage/>*/}
+      {/*<ForgetPwPage/>*/}
     </>
   )
 }
