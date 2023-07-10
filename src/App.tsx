aec6d877250a7a66270f7e520e45851ec761fd38
hyeonjin <11ghyeonjin@gachon.ac.kr>
2023-07-10 14:20:28 +0900

    BW-21 / 이슈보드 - 이슈보드 페이지 레이아웃 구현
    Navbar : 서비스 대부분에서 사용하는 네비게이션 바
    MainNavbar : 메인 페이지 고유 네비게이션 바
    IssueBoardPage : 이슈보드 페이지
    IssueCard : 이슈 카드
    IssueBadge: 이슈 뱃지
    Issue : 이슈 관련 타입 정의 .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import './assets/index.css';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SingupPage';
 import ForgetPwPage from './pages/ForgetPwPage';
+import MainNavbar from './components/MainNavbar';
+import IssueBoardPage from './pages/IssueBoardPage';
+import IssueBadge from './components/IssueBadge';
+import IssueCard from './components/IssueCard';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Navbar/> */}
-      <LoginPage/>
-      {/* <SignupPage/> */}
-      <ForgetPwPage/> 
+      {/* <Navbar/>
+      <MainNavbar/> */}
+      {/* <LoginPage/>
+      <SignupPage/>
+      <ForgetPwPage/>  */}
+      <IssueBoardPage/>
+      {/* <IssueCard/> */}
     </>
   )
 }
