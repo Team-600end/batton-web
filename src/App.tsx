1894b1bc90384b5a22ff2e8b4d397ae2e59e83c0
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2023-07-11 15:02:08 +0900

    BW-28 이슈 레포트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 // import { useState } from 'react';
 import Navbar from "./components/Navbar";
 import "./assets/index.css";
+import WriteIssueReport from "./pages/WriteIssueReport";
+import Release from "./pages/Release";
+import IssueBoardPage from "./pages/IssueBoardPage";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <WriteIssueReport />
+      {/* <Release /> */}
+      {/* <IssueBoardPage /> */}
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import React, { useState, useCallback } from "react";
 import batton_logo_img from "../assets/images/batton_logo.svg";
 import Notice from "./Notice";
 
-
 export default function Navbar() {
   const [projectDropdown, setProjectDropdown] = useState(false);
   const [profileDropdown, setProfileDropdown] = useState(false);
@@ -13,22 +12,17 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white border-gray-200 fixed top-0 left-0 right-0 bg-white z-50 shadow-sm h-[8vh]">
+    <nav className="bg-white border-gray-200 fixed top-0 left-0 right-0 bg-white z-50 shadow-sm">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="#####"
           className="flex items-center"
           style={{ cursor: "pointer", marginLeft: "-2vw" }}
         >
-          <img
-            src={batton_logo_img}
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
+          <img src={batton_logo_img} className="h-8 mr-3" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap"></span>
         </a>
         <div className="flex items-center md:order-2 mr-3">
-
           {/*알람 버튼*/}
           <button
             id="dropdownNotificationButton"
@@ -37,7 +31,13 @@ export default function Navbar() {
             type="button"
             onClick={handleDropdownToggle}
           >
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 14 20"
+            >
               <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
             </svg>
             <div className="relative flex">
@@ -215,10 +215,8 @@ export default function Navbar() {
       </div>
       {isDropdownVisible && (
         <div className="absolute right-16 w-[410px] bg-white">
-        <div className="p-4">
-          {<Notice/>}
+          <div className="p-4">{<Notice />}</div>
         </div>
-      </div>
       )}
     </nav>
   );
