5662d9f5e2d1cf6828d58d8b44a7a7e58d923380
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2023-07-17 11:01:38 +0900

    Update Release.tsx

--- src/pages/project/release/Release.tsx
+++ src/pages/project/release/Release.tsx
@@ -3,6 +3,28 @@ import Navbar from "@components/nav/Navbar";
 import block from "@assets/images/releaseBlockImg.svg";
 import ProjectNavbar from "@src/components/nav/ProjectNavbar";
 import MilestoneNavbar from "@src/components/nav/MilestoneNavbar";
+import IssueBadge from "@src/components/IssueBadge";
+import { IssueType } from "@src/types/Issue";
+
+interface DummyData {
+  issueType: IssueType;
+  description: string;
+}
+
+const dummyData: DummyData[] = [
+  {
+    issueType: "Changed",
+    description: "포트폴리오 영상 3분 이내 등록 가능1",
+  },
+  {
+    issueType: "Deprecated",
+    description: "포트폴리오 영상 3분 이내 등록 가능2",
+  },
+  {
+    issueType: "Deprecated",
+    description: "포트폴리오 영상 3분 이내 등록 가능3",
+  },
+];
 
 export default function Release() {
   return (
@@ -30,61 +52,39 @@ export default function Release() {
               <p className="text-4xl font-suitB text-black dark:text-white mb-3">
                 v.2.7.14
               </p>
-              <p className="text-xl font-suitB text-gray-900 mt-5 dark:text-white">
+              <p
+                className="text-xl font-suitB text-black dark:text-white mb-3"
+                style={{ alignSelf: "flex-end" }}
+              >
                 2023. 07. 05
               </p>
             </div>
             <hr className="h-1.5 mb-5 bg-green-500 border-0 dark:bg-gray-700"></hr>
-            <div className="flex flex-row my-2">
-              <div className="flex flex-col">
-                <span className="bg-yellow-100 text-yellow-800 text-xs font-medium mr-3 my-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 border border-yellow-300">
-                  Changed
-                </span>
-                <span className="bg-gray-100 text-gray-800 text-xs font-medium mr-3 my-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 border border-gray-300">
-                  Deprecated
-                </span>
-                <span className="bg-gray-100 text-gray-800 text-xs font-medium mr-3 my-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 border border-gray-300">
-                  Deprecated
-                </span>
-              </div>
-              <div className="flex flex-col">
-                <p className="text-base font-medium text-gray-900 dark:text-white my-2">
-                  포트폴리오 영상 3분 이내 등록 가능
-                </p>
-                <p className="text-base font-medium text-gray-900 dark:text-white my-2">
-                  포트폴리오 영상 3분 이내 등록 가능
-                </p>
-                <p className="text-base font-medium text-gray-900 dark:text-white my-2">
-                  포트폴리오 영상 3분 이내 등록 가능
-                </p>
-              </div>
-            </div>
-            {/* <div className="flex flex-row my-2">
-              <span className="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 border border-yellow-300">
-                Changed
-              </span>
-              <p className="text-base font-suitM text-black dark:text-white">
-                포트폴리오 영상 3분 이내 등록 가능
-              </p>
-            </div>
-            <hr className="h-px my-0 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-            <div className="flex flex-row my-2">
-              <span className="bg-gray-100 text-gray-800 text-xs font-suitM mr-2 px-2.5 py-0.5 rounded border border-gray-300">
-                Deprecated
-              </span>
-              <p className="text-base font-suitM text-black dark:text-white">
-                포트폴리오 영상 3분 이내 등록 가능
-              </p>
-            </div>
-            <hr className="h-px my-0 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-            <div className="flex flex-row my-2">
-              <span className="bg-gray-100 text-gray-800 text-xs font-suitM mr-2 px-2.5 py-0.5 rounded border border-gray-300">
-                Deprecated
-              </span>
-              <p className="text-base font-suitM text-black dark:text-white">
-                포트폴리오 영상 3분 이내 등록 가능
-              </p>
-            </div> */}
+            {dummyData.map((item, index) => (
+              <>
+                <div className="flex flex-row my-2" key={index}>
+                  <div className="w-24 flex justify-center">
+                    <IssueBadge issueType={item.issueType} />
+                  </div>
+                  <p className="text-base font-suitM text-black dark:text-white">
+                    {item.description}
+                  </p>
+                </div>
+                <hr className="h-px my-0 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+              </>
+            ))}
+            <p className="text-3xl font-suitSB text-black dark:text-white my-6">
+              포트폴리오 영상 3분 이상 등록 가능
+            </p>
+            <p className="text-xl font-suitM text-black dark:text-white mb-5">
+              포트폴리오 영상 길이 제한이 기존 3분 이하에서 3분 이상으로
+              변경되었습니다.
+            </p>
+            <img
+              src={block}
+              className="w-600 h-221 mt-4 ml-10"
+              alt="Flowbite Logo"
+            />
           </div>
         </div>
       </div>
