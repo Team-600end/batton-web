346a636b4f547e471a99647aa39b75f9a31a3102
imae <hgyim00@naver.com>
2023-08-02 11:28:07 +0900

    BW-58

--- src/components/project/issue/IssueBadgeDisabled.tsx
+++ src/components/project/issue/IssueBadgeDisabled.tsx
@@ -6,8 +6,8 @@ type IssueBadgeProps = {
 
 export default function IssueBadgeDisabled(props: IssueBadgeProps) {
   return (
-    <span style={{ backgroundColor: '#F4F4F4', color: '#C4C4C4', borderColor: '#C4C4C4' }} className="text-xs font-suitM mr-2 px-2.5 py-0.5 rounded-md border">
+    <span style={{ backgroundColor: "#F4F4F4", color: "#C4C4C4", borderColor: "#C4C4C4" }} className="text-xs font-suitM px-2.5 py-0.5 rounded-md border">
       {props.issueType}
     </span>
   );
-};
+}
