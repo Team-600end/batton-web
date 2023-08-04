f82a55ceae629046ed87aa3d86f89e478a9b18bb
hyeonjin <11ghyeonjin@gachon.ac.kr>
2023-08-04 16:28:58 +0900

    modified / [IssueCard]

--- src/components/project/issue/IssueCard.tsx
+++ src/components/project/issue/IssueCard.tsx
@@ -4,41 +4,50 @@ import { Issue } from "@typess/Issue";
 import kebap_img from "@assets/images/icons/kebap_black.svg";
 import default_avatar_img from "@images/common/default_profile.svg";
 import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { projectNavs } from "@src/state/projectState";
+import { ProjectNav } from "@src/types/project";
 
 type IssueCardProps = {
   issue: Issue;
 };
 
 export default function IssueCard(props: IssueCardProps) {
+  // 프로젝트 recoil
+  const [projectNav, setProjectNav] = useRecoilState(projectNavs);
+  const { projectKey } = useParams();
+  const pj = projectNav.find((element: ProjectNav) => element.projectKey.toString() == projectKey);
+
+
   const outside = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
-  const { projectKey } = useParams();
+
   const [cardDd, setCardDd] = useState(false);
 
   return (
     <div>
       <div className="rounded-md border border-gray-300 w-[16.5vw] h-[14vh] mb-[2vh] p-[1vw] shadow bg-white">
         <div className="flex items-center justify-between mb-[1vh]">
-          <IssueBadge issueType={props.issue.type} />
+          <IssueBadge issueType={props.issue.issueTag} />
           <img
             src={kebap_img}
             onClick={() => setCardDd(!cardDd)}
             style={{ marginRight: "-0.3vw", cursor: "pointer" }}
           />
         </div>
-        <p className="text-sm font-suitM">{props.issue.title}</p>
+        <p className="text-sm font-suitM">{props.issue.issueTitle}</p>
         <div className="flex items-center justify-between mt-[1.7vh]">
           <p className="text-xs font-suitM text-gray-400">
-            {props.issue.team}-{props.issue.id}
+            {pj.projectTitle}-{props.issue.issueKey}
           </p>
-          {props.issue.manager && (
+          {props.issue.nickname && (
             <img
-              src={props.issue.manager?.profileImg ?? default_avatar_img}
+              src={props.issue.profileImage ?? default_avatar_img}
               className="w-[25px] h-[25px] rounded-full object-cover ml-[6vw]"
             />
           )}
           <p className="text-xs font-suitM text-gray-700">
-            {props.issue.manager?.name}
+            {props.issue.nickname}
           </p>
         </div>
       </div>
