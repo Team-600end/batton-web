a47a16b596e30ba830d40339e94b6593418696d1
hyeonjin <11ghyeonjin@gachon.ac.kr>
2023-08-07 16:59:03 +0900

    Update IssueBadgeDisabled.tsx

--- src/components/project/issue/IssueBadgeDisabled.tsx
+++ src/components/project/issue/IssueBadgeDisabled.tsx
@@ -1,25 +1,3 @@
-// import { IssueType } from "@typess/Issue";
-
-// type IssueBadgeProps = {
-//   issueType: IssueType;
-//   onClick: () => void;
-//   onMouseEnter: () => void;
-//   onMouseLeave: () => void;
-// };
-
-// export default function IssueBadgeDisabled(props: IssueBadgeProps) {
-//   return (
-//     <span
-//       style={{ backgroundColor: "#F4F4F4", color: "#C4C4C4", borderColor: "#C4C4C4" }}
-//       className="text-xs font-suitM px-2.5 py-0.5 rounded-md border"
-//       onClick={props.onClick}
-//       onMouseEnter={props.onMouseEnter}
-//       onMouseLeave={props.onMouseLeave}
-//     >
-//       {props.issueType}
-//     </span>
-//   );
-// }
 import { IssueType } from "@typess/Issue";
 
 type IssueBadgeProps = {
@@ -27,6 +5,26 @@ type IssueBadgeProps = {
 };
 
 export default function IssueBadgeDisabled(props: IssueBadgeProps) {
+  let text = "";
+
+  switch (props.issueType) {
+    case "NEW":
+      text = "New";
+      break;
+    case "CHANGED":
+      text = "Changed";
+      break;
+    case "FEATURE":
+      text = "Feature";
+      break;
+    case "FIXED":
+      text = "Fixed";
+      break;
+    case "DEPRECATED":
+      text = "Deprecated";
+      break;
+  }
+
   return (
     <span style={{ backgroundColor: "#F4F4F4", color: "#C4C4C4", borderColor: "#C4C4C4" }} className="text-xs font-suitM px-2.5 py-0.5 rounded-md border">
       {props.issueType}
