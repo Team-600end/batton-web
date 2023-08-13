e6f20b306fb4a36d7047e4b706aedff58dae42d9
lsh9295 <lsh929500@gmail.com>
2023-08-14 03:24:30 +0900

    https test

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: './',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), mkcert()],
   define: {
     global: {},
   },
   server: {
+    host: true,
+    https: true,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
