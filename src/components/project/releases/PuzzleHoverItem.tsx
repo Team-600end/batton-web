d2d1186263f88a1cead53c2458aed74a3471e29d
imae <hgyim00@naver.com>
2023-08-17 18:15:13 +0900

    08170615

--- src/components/project/releases/PuzzleHoverItem.tsx
+++ src/components/project/releases/PuzzleHoverItem.tsx
@@ -4,10 +4,10 @@ import { Puzzle } from "@typess/Release";
 
 type PuzzleHoverProps = {
   puzzle: Puzzle;
-}
+};
 
 export default function PuzzleHoverItem(props: PuzzleHoverProps) {
-  const formattedDate = props.puzzle.date;
+  const formattedDate = props.puzzle.createdDate;
 
   return (
     <>
@@ -15,7 +15,9 @@ export default function PuzzleHoverItem(props: PuzzleHoverProps) {
         className="bg-white border-none rounded-xl shadow-xl opacity-80 mx-[20px] my-[-20px] w-min p-1 items-center justify-center"
         style={{ position: "relative", zIndex: 1 }}
       >
-        <p className="text-center">v.{props.puzzle.versionMajor}.{props.puzzle.versionMinor}.{props.puzzle.versionPatch}</p>
+        <p className="text-center">
+          v.{props.puzzle.versionMajor}.{props.puzzle.versionMinor}.{props.puzzle.versionPatch}
+        </p>
         <p className="text-center text-xs">{formattedDate}</p>
         {props.puzzle.issueList.map((issue, index) => (
           <div key={index} className="flex flex-row w-auto my-1">
